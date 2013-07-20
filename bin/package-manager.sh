4a8087edc1d8e23c476f612025091c8044eab669
Jorgen Evens <jorgen@evens.eu>
2013-07-21 00:52:08 +0200

    Wrapped find command

--- bin/package-manager.sh
+++ bin/package-manager.sh
@@ -46,7 +46,7 @@ if [ $? -eq 0 ]; then
 fi
 
 # Check the age of the current package list
-find "${PACKAGE_LIST}" -mmin +${PACKAGE_LIST_MAX_AGE} 2>&1 /dev/null
+$(find "${PACKAGE_LIST}" -mmin +${PACKAGE_LIST_MAX_AGE}) 2> /dev/null
 if [ $? -eq 0 ]; then
 	package_update_repo
 fi
